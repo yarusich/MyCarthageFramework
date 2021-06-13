aba7742b2d423be7edcda8783db90687e04dbc9f
antonsafronov <yarusich@yandex.ru>
2021-06-13 05:03:36 +0300

    Создал фреймворк

---
+++ MyCarthageFramework/MyClass.swift
@@ -0,0 +1,15 @@
+//
+//  MyClass.swift
+//  MyCarthageFramework
+//
+//  Created by Антон Сафронов on 13.06.2021.
+//
+
+import Foundation
+
+class MyClass {
+    
+    func hello() {
+        print("Hello World!")
+    }
+}
